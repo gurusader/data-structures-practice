ddf30f7dc480c72b3613ddb930810290eba22f80
gurusader <gurusader@gmail.com>
2017-02-18 13:56:22 +0900

    ArrayStack 수정.

--- ArrayStack/array_stack.c
+++ ArrayStack/array_stack.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include "array_stack.h"
@@ -9,32 +10,43 @@ void initStack(Stack* p_stack)
 
 bool isEmpty(Stack* p_stack)
 {
-	return (p_stack->top_index == -1) ? true : false;
+	return (p_stack->top_index == -1);
 }
 
 void push(Stack* p_stack, StackData data)
 {
-	p_stack->top_index++;
-	p_stack->array[p_stack->top_index] = data;
+	if (p_stack->top_index >= STACK_MAX_LENGTH - 1)
+	{
+		puts("Stack is full.");
+	}
+	else
+	{
+		p_stack->top_index++;
+		p_stack->array[p_stack->top_index] = data;
+	}
 }
 
 StackData pop(Stack* p_stack)
 {
 	if (isEmpty(p_stack))
 	{
+		puts("Stack is empty.");
 		exit(-1);
 	}
+	else
+	{
+		StackData data = p_stack->array[p_stack->top_index];
+		p_stack->top_index--;
 
-	StackData data = p_stack->array[p_stack->top_index];
-	p_stack->top_index--;
-
-	return data;
+		return data;
+	}
 }
 
 StackData peek(Stack* p_stack)
 {
 	if (isEmpty(p_stack))
 	{
+		puts("Stack is empty.");
 		exit(-1);
 	}
 
