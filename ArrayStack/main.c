4c47ff89f177e31007613ad828a3051dcdefe833
gurusader <gurusader@gmail.com>
2017-02-18 14:00:07 +0900

    ArrayStack test 추가.

---
+++ ArrayStack/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "array_stack.h"
+
+int main()
+{
+	Stack stack;
+	initStack(&stack);
+
+	push(&stack, 2);
+	push(&stack, 4);
+	push(&stack, 6);
+	push(&stack, 8);
+
+	printf("%d\n", peek(&stack));
+
+	while (!isEmpty(&stack))
+	{
+		printf("%d ", pop(&stack));
+	}
+
+	puts("");
+
+	system("pause");
+
+	return 0;
+}
