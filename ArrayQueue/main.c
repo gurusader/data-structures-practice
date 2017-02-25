6e9b6cec88a32871e258ba24c13b7485db0a41b0
gurusader <gurusader@gmail.com>
2017-02-25 12:09:42 +0900

    ArrayQueue test 추가.

---
+++ ArrayQueue/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "array_queue.h"
+
+int main()
+{
+	Queue queue;
+	initQueue(&queue);
+
+	enQueue(&queue, 2);
+	enQueue(&queue, 4);
+	enQueue(&queue, 6);
+	enQueue(&queue, 8);
+
+	printf("%d\n", peek(&queue));
+
+	while (!isEmpty(&queue))
+	{
+		printf("%d ", deQueue(&queue));
+	}
+
+	puts("");
+
+	system("pause");
+
+	return 0;
+}
